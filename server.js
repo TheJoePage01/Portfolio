874c3ad6628b840490c27ba3f0b584fc6fa5f147
AlexSteiner30 <65250433+AlexSteiner30@users.noreply.github.com>
2023-01-02 11:53:20 +0100

    About Start

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('/', function(req, res){
     res.render('index');
 });
 
+app.get('/about', function(req, res){
+    res.render('about');
+});
+
 app.get('/projects', function(req, res){
     res.render('projects');
 });
