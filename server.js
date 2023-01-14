df332fbeb27d1be76e63c1c3b8f4ee3b1e6be25c
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-01-14 15:00:36 +0100

    Reload Posts Fix

--- server.js
+++ server.js
@@ -1,37 +1,89 @@
-const blog = require('./blog.js')
+const mongoose = require('mongoose');
 
 const express = require('express');
 const app = express();
 
+require('dotenv').config();
+
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
+// Index
 app.get('/', function(req, res){
     res.render('index');
 });
 
+// About
 app.get('/about', function(req, res){
     res.render('about');
 });
 
+// Projects
 app.get('/projects', function(req, res){
     res.render('projects');
 });
 
+// Contact
 app.get('/contact', function(req, res){
     res.render('contact');
 });
 
-blog.Post.find({}, function(err, posts) {
-    if(!err){
-        posts.forEach(post => {
-            app.get(`/blog/${post._id}`, function(req, res){
-                res.send(post._id);
-            });
-        });
-    }
+// Mongoose Connection
+async function Connect() {
+    mongoose.set('strictQuery', true);
+    await mongoose.connect(process.env.MONGO_STRING);
+
+    console.log('Connected to database!');
+}
+
+// Schema
+const postSchema = new mongoose.Schema({
+    title : String,
+    subTitle : String,
+    date : String, // need to change it to date format
+    author : String, 
+    text : String,
+    arguments : String
 });
 
-app.listen(port=process.env.PORT, function(){
+const Post = mongoose.model('post', postSchema);
+
+// Functions
+function ReloadPosts(){
+    Post.find({}, function(err,posts) {
+        if(!err){
+            posts.forEach(post => {
+                app.get(`/blog/${post._id}`, function(req, res){
+                    res.render('post', {
+                        post : post
+                    });
+                });
+            });
+
+            console.log('All posts were updated!');
+        }
+    });
+}
+
+function UploadNewPost(title, subTitle, author, text, arguments){
+    const newPost = new Post({
+        title : title,
+        subTitle : subTitle,
+        date : "1/1/2023", // need to change it to a date
+        author : author, 
+        text : text,
+        arguments : arguments
+    });
+
+    newPost.save();
+
+    console.log(`A new post has been added! Check it out! http://alexsteiner.dev/blog/${newPost._id}`);
+}
+
+
+// Run
+app.listen(port=process.env.PORT, async function(){
     console.log('Server running on port ' + process.env.PORT);
+    Connect().catch(err => console.log(err));
+    ReloadPosts();
 });
